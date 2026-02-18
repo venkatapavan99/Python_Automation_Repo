29d5b591f6f7054f1914f4bd7883fecec8161c2c
Mareedu Venkata Pavan <84392859+venkatapavan99@users.noreply.github.com>
2026-02-19 00:52:52 +0530

    Update Chrome driver initialization for headless mode

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -1,13 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
 def get_driver():
     chrome_options = Options()
-    chrome_options.add_argument("--headless")  # Important for CI
+    chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1920,1080")
 
-    driver = webdriver.Chrome(options=chrome_options)
+    service = Service("/usr/bin/chromedriver")
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+
     driver.implicitly_wait(10)
     return driver
-
